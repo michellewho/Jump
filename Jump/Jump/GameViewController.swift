38f46f1e5e14d448193fd5de0d2b1142c72b9d49
Michelle Ho <michelleho@Michelles-MacBook-Pro.local>
2019-04-09 00:23:41 -0700

    start project from scratch

---
+++ Jump/Jump/GameViewController.swift
@@ -0,0 +1,32 @@
+//
+//  GameViewController.swift
+//  Jump
+//
+//  Created by Michelle Ho on 4/9/19.
+//  Copyright © 2019 Michelle Ho. All rights reserved.
+//
+
+import UIKit
+import SpriteKit
+
+class GameViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let view = self.view as! SKView? {
+            let scene = GameScene(size: view.bounds.size)
+            // Set the scale mode to scale to fit the window
+            scene.scaleMode = .aspectFill
+            
+            // Present the scene
+            view.presentScene(scene)
+            
+            
+            view.ignoresSiblingOrder = true
+            
+            view.showsFPS = true
+            view.showsNodeCount = true
+        }
+    }
+}
